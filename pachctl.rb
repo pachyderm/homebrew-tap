8cca5eb53e3fd9c08eb40c6aef816e7ab02be84b
Sean Jezewski <seanwjezewski@gmail.com>
2016-04-19 12:54:28 -0700

    Bump to version v1.0.0(530) w a proper git download URL

--- pachctl.rb
+++ pachctl.rb
@@ -5,8 +5,8 @@ require 'erb'
 
 class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/archive/v1.0.0.522.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/pachyderm/pachyderm/archive/v1.0.0(530).tar.gz"
+  sha256 "309c2b7f12b4941b1941bb059865e44a283cdb03d3e6bcc54d63ae7eb0ba4420"
 
   depends_on "go" => :build
 
