969bf229cd2ef331c353ba6e9bff89274739e075
Sean Jezewski <seanwjezewski@gmail.com>
2016-04-19 12:13:12 -0700

    Update to latest v1.0.0(522) and use new versioning format

--- pachctl.rb
+++ pachctl.rb
@@ -5,8 +5,8 @@ require 'erb'
 
 class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/archive/v1.0.498.tar.gz"
-  sha256 "7e48fe572babbe0c45285b19fe58dcd1d8641212656a925282f87169cb6b300d"
+  url "https://github.com/pachyderm/pachyderm/archive/v1.0.0.522.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   depends_on "go" => :build
 
