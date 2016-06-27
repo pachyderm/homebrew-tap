31c9200885db58cb433101888a2484e1acd75012
Sean Jezewski <seanwjezewski@gmail.com>
2016-06-27 16:15:49 -0700

    [Automated] Update formula to release version 1.0.1-RCX

--- pachctl.rb
+++ pachctl.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.0.1-dirty/pachctl_v1.0.1-dirty_darwin_amd64.zip"
-  sha256 "1ce0a016d00d1ee7bef8aaefe824781602d26890076ee8d1bfa1b470e609c923"
-  version "v1.0.1-dirty"
+  url "https://github.com/pachyderm/pachyderm/releases/download/1.0.1-RCX/pachctl_1.0.1-RCX_darwin_amd64.zip"
+  sha256 ""
+  version "1.0.1-RCX"
 
   def install
     bin.install buildpath/"pachctl"
