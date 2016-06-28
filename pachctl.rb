c7b524cea8ffa5d076177fc7a34f88d77771d5a4
Sean Jezewski <seanwjezewski@gmail.com>
2016-06-28 15:32:11 -0700

    [Automated] Update formula to release version 0.0.9999

--- pachctl.rb
+++ pachctl.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.0.1-RCX19/pachctl_1.0.1-RCX19_darwin_amd64.zip"
-  sha256 "d96a526dfeea7cf52a2356ea7781e1c40abb363c2599645558f43bd4fc0faaec"
-  version "v1.0.1-RCX19"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v0.0.9999/pachctl_0.0.9999_darwin_amd64.zip"
+  sha256 "9a1db571998c0e875b07e9294a65bb3d4f69e5fb78daf747df75fbd248a61486"
+  version "v0.0.9999"
 
   def install
     bin.install buildpath/"pachctl"
