d6a310398de2df1cad5aef5cff652d45e6341b3e
Sean Jezewski <seanwjezewski@gmail.com>
2016-09-12 23:40:43 +0000

    [Automated] Update formula to release version 1.2.0-RCX6

--- pachctl.rb
+++ pachctl.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.2.0-RCX3/pachctl_1.2.0-RCX3_darwin_amd64.zip"
-  sha256 "4a6aa8d56308c65d2c95049297f5140f8adcfe807a1bbad73b4dd24a90718820"
-  version "v1.2.0-RCX3"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.2.0-RCX6/pachctl_1.2.0-RCX6_darwin_amd64.zip"
+  sha256 "2d552c6e408e2f30352b5ee831c0985fc9db65e67ba4e51049d2d15c3b99f72e"
+  version "v1.2.0-RCX6"
 
   def install
     bin.install buildpath/"pachctl"
