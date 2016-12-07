7b820d8d92f5baae93db2eee37603262a3799405
Sean Jezewski <seanwjezewski@gmail.com>
2016-12-07 14:18:46 -0800

    [Automated] Update formula to release version 1.3.0

--- pachctl.rb
+++ pachctl.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.3.0-2b908777551ccd8af08cfb23d4c57f9ad0fd4831/pachctl_1.3.0-2b908777551ccd8af08cfb23d4c57f9ad0fd4831_darwin_amd64.zip"
-  sha256 "bb82cac1043941795150ad7ef378be0989d77cf1db3f2cc663c2bf13164340e1"
-  version "v1.3.0-2b908777551ccd8af08cfb23d4c57f9ad0fd4831"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.3.0/pachctl_1.3.0_darwin_amd64.zip"
+  sha256 "de0eb28f028dace7c18d2d92bbb6f518d51e43e62255c2e80dad4a5c944be17e"
+  version "v1.3.0"
 
   def install
     bin.install buildpath/"pachctl"
