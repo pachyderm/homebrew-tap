ad57b0b63224dfe1b25b2496856a13cb997c39ac
Sean Jezewski <seanwjezewski@gmail.com>
2017-03-27 11:39:48 -0700

    Fix up update-formula.sh
    roperly generates the new formula file

--- update-formula.sh
+++ update-formula.sh
@@ -1,6 +1,6 @@
 # Full URL looks like this:
 # https://github.com/pachyderm/pachyderm/releases/download/v1.0.1-dirty/pachctl_v1.0.1-dirty_darwin_amd64.zip
-GITHUB_HOSTING_ROOT=https:\/\/github.com\/pachyderm\/pachyderm\/releases\/download
+GITHUB_HOSTING_ROOT=https://github.com/pachyderm/pachyderm/releases/download
 MAC_BINARY_SUFFIX=_darwin_amd64.zip
 FORMULA=pachctl.rb.template
 
@@ -11,24 +11,29 @@ MAJOR_MINOR_SQUASHED=`echo $MAJOR_MINOR | sed 's/\.//g'`
 
 # Linux doesn't allow a space after the -i flag. OSX needs it. For now I'll assume builds/releases are done on a linux machine
 
+# Put the file in place
+cp $FORMULA pachctl@$MAJOR_MINOR.rb
+
+FORMULA="pachctl@$MAJOR_MINOR.rb"
+
 ### replace binary URL
-sed -i'' 's/url ".*"/url "$(GITHUB_HOSTING_ROOT)\/v$(VERSION)\/pachctl_$(VERSION)$(MAC_BINARY_SUFFIX)"/g' $(FORMULA)
+sed -i'' 's@url ".*"@url "'"$GITHUB_HOSTING_ROOT"'/v'"$VERSION"'/pachctl_'"$VERSION$MAC_BINARY_SUFFIX"'"''@' $FORMULA
 
 
 ### get binary and update SHA
-curl -L -o binary "$(subst \/,/,$(GITHUB_HOSTING_ROOT))/v$(VERSION)/pachctl_$(VERSION)$(MAC_BINARY_SUFFIX)"
+DOWNLOAD_URL=$GITHUB_HOSTING_ROOT/v$VERSION/pachctl_$VERSION$MAC_BINARY_SUFFIX
+echo "download: $DOWNLOAD_URL"
+curl -L -o binary "$DOWNLOAD_URL"
 # Need sha256sum ... install on mac via: 'brew install coreutils'
 # And then do 'ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum'
 which sha256sum
 sha256sum binary | cut -f 1 -d " " > SHA
-cat SHA | xargs -I NEWSHA sed -i'' 's/sha256 ".*"/sha256 "NEWSHA"/g' $(FORMULA)
+cat SHA | xargs -I NEWSHA sed -i'' 's/sha256 ".*"/sha256 "NEWSHA"/g' $FORMULA
 
 
 ### update version
-sed -i'' 's/version ".*"/version "v$(VERSION)"/g' $(FORMULA)
-sed -i'' 's/class Pachctl/class PachctlAT$(MAJOR_MINOR_SQUASHED)"/g' $(FORMULA)
+sed -i'' 's/version ".*"/version "v'"$VERSION"'"/g' $FORMULA
+sed -i'' 's/class Pachctl/class PachctlAT'"$MAJOR_MINOR_SQUASHED"'/g' $FORMULA
 
-# Put the file in place
-mv $(FORMULA) pachctl@$(MAJOR_MINOR).rb
 
 
