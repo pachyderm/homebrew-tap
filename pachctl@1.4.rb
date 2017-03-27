bdfd6f0d27ff0bced679105471b075fcb7ee9a41
Sean Jezewski <seanwjezewski@gmail.com>
2017-03-27 16:26:27 -0700

    [Automated] Update formula to release version 1.4.0-RCtestrlsa

--- pachctl@1.4.rb
+++ pachctl@1.4.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT14 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.4.0/pachctl_1.4.0_darwin_amd64.zip"
-  sha256 "5ffb40d845d7f2410ca7d4ca0a97b06619d0ac1c5ac05f5b16f1ff727607fb79"
-  version "v1.4.0"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.4.0-RCtestrlsa/pachctl_1.4.0-RCtestrlsa_darwin_amd64.zip"
+  sha256 "c043a6133b9c19a8908d567961207015f7575c966607023f5754e0ca59980ac8"
+  version "v1.4.0-RCtestrlsa"
 
   def install
     bin.install buildpath/"pachctl"
