b99bd9d0662cb898e8232117bc13038337d3b60b
Sean Jezewski <seanwjezewski@gmail.com>
2017-03-28 12:12:51 -0700

    [Automated] Update formula to release version 1.4.0

--- pachctl@1.4.rb
+++ pachctl@1.4.rb
@@ -6,7 +6,7 @@ require 'erb'
 class PachctlAT14 < Formula
   homepage "github.com/pachyderm/pachyderm"
   url "https://github.com/pachyderm/pachyderm/releases/download/v1.4.0/pachctl_1.4.0_darwin_amd64.zip"
-  sha256 "a7cc841f9dcfb02a70cc363f27aadfdd9d3ec9190eaf20db3b30bb0f0ad955ef"
+  sha256 "471488df53abb5da0e0f31c33bcc2a2ed3b4f9fdf78fca13044fef599da36f48"
   version "v1.4.0"
 
   def install
