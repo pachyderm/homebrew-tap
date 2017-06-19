7bc8bbf8bd695ef7f72513a12f275929d0386186
Sean Jezewski <seanwjezewski@gmail.com>
2017-06-19 16:22:47 -0700

    [Automated] Update formula to release version 1.4.8-pici-custom-1

--- pachctl@1.4.rb
+++ pachctl@1.4.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT14 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.4.8/pachctl_1.4.8_darwin_amd64.zip"
-  sha256 "0af641a28713bcea2b656fdeebabc284b3ccb10229a0e202ec9792a15404ff3c"
-  version "v1.4.8"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.4.8-pici-custom-1/pachctl_1.4.8-pici-custom-1_darwin_amd64.zip"
+  sha256 "43e8eb49f02a85b6e799b93af6d58991f1b971b39aa60c649ff8983cd04f7e52"
+  version "v1.4.8-pici-custom-1"
 
   def install
     bin.install buildpath/"pachctl"
