8e96ab5d59717dc8bbd0ef25e99893015a51790a
Matthew Steffen <msteffen@pachyderm.io>
2017-09-27 12:01:20 -0700

    [Automated] Update formula to release version 1.6.0rc7

--- pachctl@1.6.rb
+++ pachctl@1.6.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT16 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.6.01.6.0rc7/pachctl_1.6.01.6.0rc7_darwin_amd64.zip"
-  sha256 "8f1559b0100651ea099b32d7cde2773c2fbbdeeed0e0340bd3a8562dc6f1efd7"
-  version "v1.6.01.6.0rc7"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.6.0rc7/pachctl_1.6.0rc7_darwin_amd64.zip"
+  sha256 "6350a2808a038287716e5d927846e071f11dcef8ae85f8a3b195ec071a5291d4"
+  version "v1.6.0rc7"
 
   def install
     bin.install buildpath/"pachctl"
