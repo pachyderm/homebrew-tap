fb005ae440e2c2cd0a6ecf82262b342827cbff4d
Matthew Steffen <msteffen@pachyderm.io>
2017-11-13 22:42:52 -0800

    [Automated] Update formula to release version 1.6.4-no-gpu-01

--- pachctl@1.6.rb
+++ pachctl@1.6.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT16 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.6.4/pachctl_1.6.4_darwin_amd64.zip"
-  sha256 "44b3938742459dc983dd218050b4754c23334c7845ad4a9ade9c82f3a76fa2c3"
-  version "v1.6.4"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.6.4-no-gpu-01/pachctl_1.6.4-no-gpu-01_darwin_amd64.zip"
+  sha256 "6d05958c66ca460be4fd0febc305827ce122a6b52435a29ab4fdba1e224408ec"
+  version "v1.6.4-no-gpu-01"
 
   def install
     bin.install buildpath/"pachctl"
