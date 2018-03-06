1a2d5da420e752038d03b0bb88534a36929ae93b
Sean Jezewski <seanwjezewski@gmail.com>
2018-03-06 13:42:28 -0800

    Revert "[Automated] Update formula to release version 1.7.0-custom-02032018"
    This reverts commit 680de8b4a7195a699c1d3a8a8b9d5c7e319f496b.
    
    We don't want the custom release to occlude the 1.7.0-rc2 release

--- pachctl@1.7.rb
+++ pachctl@1.7.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT17 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.7.0-custom-02032018/pachctl_1.7.0-custom-02032018_darwin_amd64.zip"
-  sha256 "05e9ad4e4484c70212f813f4e32bf4ebbbc5ad58e879c71dc86165484ce140db"
-  version "v1.7.0-custom-02032018"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.7.0rc2/pachctl_1.7.0rc2_darwin_amd64.zip"
+  sha256 "4aa565a6ec2a1d16834797c24051a93a0554e8af7163a1e1a30934a620658985"
+  version "v1.7.0rc2"
 
   def install
     bin.install buildpath/"pachctl"
