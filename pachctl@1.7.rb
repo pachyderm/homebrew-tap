43d6091364dd1768808a5ee317907671d3885eae
Matthew Steffen <msteffen@pachyderm.io>
2018-03-21 13:18:53 -0700

    [Automated] Update formula to release version 1.7.0rc4

--- pachctl@1.7.rb
+++ pachctl@1.7.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT17 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.7.0rc2/pachctl_1.7.0rc2_darwin_amd64.zip"
-  sha256 "4aa565a6ec2a1d16834797c24051a93a0554e8af7163a1e1a30934a620658985"
-  version "v1.7.0rc2"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.7.0rc4/pachctl_1.7.0rc4_darwin_amd64.zip"
+  sha256 "bdda7e782908f8018b2eb04680a01fd45de9b8cf52e6ab14e7481dee907f1fdd"
+  version "v1.7.0rc4"
 
   def install
     bin.install buildpath/"pachctl"
