872b64c1adec91afa1e555778862664a108116a0
Joe Doliner <jdoliner@pachyderm.io>
2018-03-28 11:34:49 -0700

    Fix broken 1.7.0 release.

--- pachctl@1.7.rb
+++ pachctl@1.7.rb
@@ -6,8 +6,8 @@ require 'erb'
 class PachctlAT17 < Formula
   homepage "github.com/pachyderm/pachyderm"
   url "https://github.com/pachyderm/pachyderm/releases/download/v1.7.0rc2/pachctl_1.7.0rc2_darwin_amd64.zip"
-  sha256 "4aa565a6ec2a1d16834797c24051a93a0554e8af7163a1e1a30934a620658985"
-  version "v1.7.0rc2"
+  sha256 "3bcd391021b507f8e38f1d1b1ac45ecebd49352901959bfb51f63c1059c6e484"
+  version "v1.7.0"
 
   def install
     bin.install buildpath/"pachctl"
