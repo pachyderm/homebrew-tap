0082251326a1b4209361784e6f9b24966f575cc9
Joe Doliner <jdoliner@pachyderm.io>
2018-03-28 11:43:31 -0700

    Fix it again.

--- pachctl@1.7.rb
+++ pachctl@1.7.rb
@@ -5,7 +5,7 @@ require 'erb'
 
 class PachctlAT17 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.7.0rc2/pachctl_1.7.0rc2_darwin_amd64.zip"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.7.0/pachctl_1.7.0_darwin_amd64.zip"
   sha256 "3bcd391021b507f8e38f1d1b1ac45ecebd49352901959bfb51f63c1059c6e484"
   version "v1.7.0"
 
