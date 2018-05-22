17ddd710460092bcc0094ed6d2cddb09fb3cb7a7
Joe Doliner <jdoliner@gmail.com>
2018-05-22 12:40:23 -0700

    [Automated] Update formula to release version 1.7.2

--- pachctl@1.7.rb
+++ pachctl@1.7.rb
@@ -6,7 +6,7 @@ require 'erb'
 class PachctlAT17 < Formula
   homepage "github.com/pachyderm/pachyderm"
   url "https://github.com/pachyderm/pachyderm/releases/download/v1.7.2/pachctl_1.7.2_darwin_amd64.zip"
-  sha256 "89b4612bcf1cc86f889c36f66ee25ac8732438a2f84bcb1b81beff7b065d22cd"
+  sha256 "814d914d18f4d5fdf95a0093cd5a0117c3b66389c21f3db7994f05688fc1d352"
   version "v1.7.2"
 
   def install
