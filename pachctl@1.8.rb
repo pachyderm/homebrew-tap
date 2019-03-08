0247808edc80cac1e9b172a8717f1eff65e12454
Joe Doliner <jdoliner@gmail.com>
2019-03-08 13:24:31 -0800

    [Automated] Update formula to release version 1.8.6

--- pachctl@1.8.rb
+++ pachctl@1.8.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT18 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.8.5/pachctl_1.8.5_darwin_amd64.zip"
-  sha256 "3bfd5a9c3fd0f14e987ad1b2fd54ef3a4162c33826c56b205d5e66216be3f1ce"
-  version "v1.8.5"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.8.6/pachctl_1.8.6_darwin_amd64.zip"
+  sha256 "2fcf09d443d4b530eda02b73380e871a29e7579cef4c3e61ea48fc235c8c4af7"
+  version "v1.8.6"
 
   def install
     bin.install buildpath/"pachctl"
