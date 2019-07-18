3384ba5bbc835fcbeba56c935e020dfa51b1a051
Joe Doliner <jdoliner@gmail.com>
2019-07-18 15:31:33 -0700

    Update pachctl@1.9.rb
    Fix sha256 value.

--- pachctl@1.9.rb
+++ pachctl@1.9.rb
@@ -6,7 +6,7 @@ require 'erb'
 class PachctlAT19 < Formula
   homepage "github.com/pachyderm/pachyderm"
   url "https://github.com/pachyderm/pachyderm/releases/download/v1.9.1/pachctl_1.9.1_darwin_amd64.zip"
-  sha256 "d35482af055d545f3b0ebd7a4ff7e1369414853066e8dc01a15ea66b63b8a0c6"
+  sha256 "edc4d724644ddb12c97819b62bd9fdbd107fe32588a62cc8253e35ca00118857"
   version "v1.9.1"
 
   def install
