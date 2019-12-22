4b094c6eefacffc5736f8627cf2aee4fc3f10bc2
Joe Doliner <jdoliner@gmail.com>
2019-12-22 00:20:13 -0800

    [Automated] Update formula to release version 1.9.9-1b3f8868a1f7e75af2cb2788ac7d7a79c59c3ef8

--- pachctl@1.9.rb
+++ pachctl@1.9.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT19 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.9.10/pachctl_1.9.10_darwin_amd64.zip"
-  sha256 "561fb4b76659b0062a3b1ef0d0e374a7014d7fbe94aacf34987fcb2b5c318b08"
-  version "v1.9.10"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.9.9-1b3f8868a1f7e75af2cb2788ac7d7a79c59c3ef8/pachctl_1.9.9-1b3f8868a1f7e75af2cb2788ac7d7a79c59c3ef8_darwin_amd64.zip"
+  sha256 "379de4d58378e3b6f483d393137df688c606c2d0b68576f8692381e4d5982b11"
+  version "v1.9.9-1b3f8868a1f7e75af2cb2788ac7d7a79c59c3ef8"
 
   def install
     bin.install buildpath/"pachctl"
