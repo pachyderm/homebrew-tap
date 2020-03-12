f8983e978900c131fe52e5166a3a8882df36b7a7
Joe Doliner <jdoliner@gmail.com>
2020-03-11 17:31:22 -0700

    [Automated] Update formula to release version 1.10.0

--- pachctl@1.10.rb
+++ pachctl@1.10.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT110 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.10.0-rc5/pachctl_1.10.0-rc5_darwin_amd64.zip"
-  sha256 "511a9b0f7e29304a2934664bacbc75bf5a3d9e86644787bdac59e976f3f0e7ea"
-  version "v1.10.0-rc5"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.10.0/pachctl_1.10.0_darwin_amd64.zip"
+  sha256 "bd691f0bb24bed7b8d987f07bc730367d87c3636158ca702d3891805b36b39dc"
+  version "v1.10.0"
 
   def install
     bin.install buildpath/"pachctl"
