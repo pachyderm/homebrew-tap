1e77255f0925e750d0e287391e3571d30869a588
Joe Doliner <jdoliner@gmail.com>
2020-07-06 13:12:06 -0700

    [Automated] Update formula to release version 1.11.0-sentry1

--- pachctl@1.11.rb
+++ pachctl@1.11.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT111 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.11.0-rc1/pachctl_1.11.0-rc1_darwin_amd64.zip"
-  sha256 "2b665aafd697865db599114cb3f895164cca9b45f0db0a3b2d78dcea16715dc6"
-  version "v1.11.0-rc1"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.11.0-sentry1/pachctl_1.11.0-sentry1_darwin_amd64.zip"
+  sha256 "f4c44c4d0611f338c8a4c43c9146c38111344d0c1974810494cce206beef46bb"
+  version "v1.11.0-sentry1"
 
   def install
     bin.install buildpath/"pachctl"
