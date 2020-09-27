32959c38240651614ca7498d4473fcf81c7023c5
Matthew Steffen <msteffen@pachyderm.io>
2020-09-26 22:39:50 -0700

    [Automated] Update formula to release version 1.11.3-s3-logging-20200926-2121

--- pachctl@1.11.rb
+++ pachctl@1.11.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT111 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.11.3/pachctl_1.11.3_darwin_amd64.zip"
-  sha256 "0cda396a41953e959c21fee7f35f6c921bdd870397f405a2f30f048c96aee180"
-  version "v1.11.3"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.11.3-s3-logging-20200926-2121/pachctl_1.11.3-s3-logging-20200926-2121_darwin_amd64.zip"
+  sha256 "44dd33bb2392bd5305c26c783b8018bebb50bcdc17b39731b6bc90dd91eeb5b0"
+  version "v1.11.3-s3-logging-20200926-2121"
 
   def install
     bin.install buildpath/"pachctl"
