454ecbb3c8ad2c778baad63de040c234570a109a
nitinjainsj <nitin@pachyderm.com>
2020-12-07 20:26:02 -0800

    Update formula to support mac-os

--- update-formula.sh
+++ update-formula.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Full URL looks like this:
 # https://github.com/pachyderm/pachyderm/releases/download/v1.0.1-dirty/pachctl_v1.0.1-dirty_darwin_amd64.zip
 GITHUB_HOSTING_ROOT=https://github.com/pachyderm/pachyderm/releases/download
@@ -9,15 +11,19 @@ MAJOR_MINOR=`echo $VERSION | cut -f -2 -d "."`
 # e.g. convert from 1.3 -> 13
 MAJOR_MINOR_SQUASHED=`echo $MAJOR_MINOR | sed 's/\.//g'`
 
-# Linux doesn't allow a space after the -i flag. OSX needs it. For now I'll assume builds/releases are done on a linux machine
-
-# Put the file in place
 cp $FORMULA pachctl@$MAJOR_MINOR.rb
 
 FORMULA="pachctl@$MAJOR_MINOR.rb"
 
+# Linux doesn't allow a space after the -i flag in sed. OSX needs it.
+
+# Put the file in place
 ### replace binary URL
-sed -i'' 's@url ".*"@url "'"$GITHUB_HOSTING_ROOT"'/v'"$VERSION"'/pachctl_'"$VERSION$MAC_BINARY_SUFFIX"'"''@' $FORMULA
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' 's@url ".*"@url "'"$GITHUB_HOSTING_ROOT"'/v'"$VERSION"'/pachctl_'"$VERSION$MAC_BINARY_SUFFIX"'"''@' $FORMULA
+else
+  sed -i'' 's@url ".*"@url "'"$GITHUB_HOSTING_ROOT"'/v'"$VERSION"'/pachctl_'"$VERSION$MAC_BINARY_SUFFIX"'"''@' $FORMULA
+fi
 
 
 ### get binary and update SHA
@@ -28,12 +34,18 @@ curl -L -o binary "$DOWNLOAD_URL"
 # And then do 'ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum'
 which sha256sum
 sha256sum binary | cut -f 1 -d " " > SHA
-cat SHA | xargs -I NEWSHA sed -i'' 's/sha256 ".*"/sha256 "NEWSHA"/g' $FORMULA
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  cat SHA | xargs -I NEWSHA sed -i '' 's/sha256 ".*"/sha256 "NEWSHA"/g' $FORMULA
+else
+  cat SHA | xargs -I NEWSHA sed -i'' 's/sha256 ".*"/sha256 "NEWSHA"/g' $FORMULA
+fi
 
 
 ### update version
-sed -i'' 's/version ".*"/version "v'"$VERSION"'"/g' $FORMULA
-sed -i'' 's/class Pachctl/class PachctlAT'"$MAJOR_MINOR_SQUASHED"'/g' $FORMULA
-
-
-
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' 's/version ".*"/version "v'"$VERSION"'"/g' $FORMULA
+  sed -i '' 's/class Pachctl/class PachctlAT'"$MAJOR_MINOR_SQUASHED"'/g' $FORMULA
+else
+  sed -i'' 's/version ".*"/version "v'"$VERSION"'"/g' $FORMULA
+  sed -i'' 's/class Pachctl/class PachctlAT'"$MAJOR_MINOR_SQUASHED"'/g' $FORMULA
+fi
