4ca73cbd41a9107924f326b80e2fdcae96e24127
nitinjainsj <nitin@pachyderm.com>
2021-02-03 17:23:35 -0800

    [Automated] Update formula to release version 1.12.2

--- pachctl@1.12.rb
+++ pachctl@1.12.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT112 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v1.12.1/pachctl_1.12.1_darwin_amd64.zip"
-  sha256 "5847df0b1cace62f052d02a97dac094341566dbefca68fbb3263bdc9c46c01be"
-  version "v1.12.1"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v1.12.2/pachctl_1.12.2_darwin_amd64.zip"
+  sha256 "bc3ebb423f638104bc7aa630e5c915066065ae0bc1ad6736e14efb3f033b0395"
+  version "v1.12.2"
 
   def install
     bin.install buildpath/"pachctl"
