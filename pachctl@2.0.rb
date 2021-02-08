ca102e8c2e948e635b7bcda6961bf1b3d539cac5
Alysha Gardner <3100188+actgardner@users.noreply.github.com>
2021-02-08 10:39:30 -0500

    Fix sha for 2.0-alpha.2

--- pachctl@2.0.rb
+++ pachctl@2.0.rb
@@ -6,7 +6,7 @@ require 'erb'
 class PachctlAT20 < Formula
   homepage "github.com/pachyderm/pachyderm"
   url "https://github.com/pachyderm/pachyderm/releases/download/v2.0.0-alpha.2/pachctl_2.0.0-alpha.2_darwin_amd64.zip"
-  sha256 "8a70c5b2e32c2bd3b560489de4af66da16fc6a6c"
+  sha256 "e141f5425329c337c1d589a08a99f82e50ce51e1df37bd78a5c505ee9afdf27e"
   version "v2.0.0-alpha.2"
 
   def install
