f744c050ded2a982502cceda6733f1f08fc08ccc
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-03-01 10:11:47 -0500

    [Automated] Update formula to release version 2.0.0-alpha.6

--- pachctl@2.0.rb
+++ pachctl@2.0.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT20 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v2.0.0-alpha.5/pachctl_2.0.0-alpha.5_darwin_amd64.zip"
-  sha256 "2f5a03ac9b33f5649b703e21eb4e3ad7c7f88b541750661462f2871f815e5b9b"
-  version "v2.0.0-alpha.5"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v2.0.0-alpha.6/pachctl_2.0.0-alpha.6_darwin_amd64.zip"
+  sha256 "e2d2ad1a95523f18dda02a68933863fa8d03ab315a7cecc470bf6c82a174d7b5"
+  version "v2.0.0-alpha.6"
 
   def install
     bin.install buildpath/"pachctl"
