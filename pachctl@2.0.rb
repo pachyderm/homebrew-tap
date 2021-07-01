62c650476aefd07f20754b4704f4237ce1b9f0cb
Bryce McAnally <bryce@pachyderm.io>
2021-07-01 11:28:00 -0500

    [Automated] Update formula to release version 2.0.0-proxy.1

--- pachctl@2.0.rb
+++ pachctl@2.0.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT20 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v2.0.0-alpha.28/pachctl_2.0.0-alpha.28_darwin_amd64.zip"
-  sha256 "f73efa7faa727d30d5f8dc64123367cf63b93b9bb3b80444fc6495ce782174d1"
-  version "v2.0.0-alpha.28"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v2.0.0-proxy.1/pachctl_2.0.0-proxy.1_darwin_amd64.zip"
+  sha256 "8b17e896eead6607db1dcb8a7c497239afe7a73c035b7accb1e22d0711f26158"
+  version "v2.0.0-proxy.1"
 
   def install
     bin.install buildpath/"pachctl"
