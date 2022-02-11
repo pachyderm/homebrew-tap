dfc81add7e074a9abffe4e74e8a3a9f31c234a27
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2022-02-11 09:11:55 -0500

    update for arm/amd

--- update-formula.sh
+++ update-formula.sh
@@ -3,7 +3,8 @@
 # Full URL looks like this:
 # https://github.com/pachyderm/pachyderm/releases/download/v1.0.1-dirty/pachctl_v1.0.1-dirty_darwin_amd64.zip
 GITHUB_HOSTING_ROOT=https://github.com/pachyderm/pachyderm/releases/download
-MAC_BINARY_SUFFIX=_darwin_amd64.zip
+MACINTEL_BINARY_SUFFIX=_darwin_amd64.zip
+MACARM_BINARY_SUFFIX=_darwin_arm64.zip
 FORMULA=pachctl.rb.template
 
 # e.g. convert from 1.3.4 -> 1.3
@@ -16,28 +17,35 @@ cp $FORMULA pachctl@$MAJOR_MINOR.rb
 FORMULA="pachctl@$MAJOR_MINOR.rb"
 
 # Linux doesn't allow a space after the -i flag in sed. OSX needs it.
-
+AMDURL=$GITHUB_HOSTING_ROOT"/v"$VERSION"/pachctl_"$VERSION$MACINTEL_BINARY_SUFFIX
+ARMURL=$GITHUB_HOSTING_ROOT"/v"$VERSION"/pachctl_"$VERSION$MACARM_BINARY_SUFFIX
 # Put the file in place
 ### replace binary URL
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's@url ".*"@url "'"$GITHUB_HOSTING_ROOT"'/v'"$VERSION"'/pachctl_'"$VERSION$MAC_BINARY_SUFFIX"'"''@' $FORMULA
+  sed -i '' -e "s|AMDURLPH|${AMDURL}|g" $FORMULA
+  sed -i '' -e "s|ARMURLPH|${ARMURL}|g" $FORMULA
 else
-  sed -i'' 's@url ".*"@url "'"$GITHUB_HOSTING_ROOT"'/v'"$VERSION"'/pachctl_'"$VERSION$MAC_BINARY_SUFFIX"'"''@' $FORMULA
+  sed -i'' -e "s|AMDURLPH|${AMDURL}|g" $FORMULA
+  sed -i'' -e "s|ARMURLPH|${ARMURL}|g" $FORMULA
 fi
 
 
 ### get binary and update SHA
-DOWNLOAD_URL=$GITHUB_HOSTING_ROOT/v$VERSION/pachctl_$VERSION$MAC_BINARY_SUFFIX
-echo "download: $DOWNLOAD_URL"
-curl -L -o binary "$DOWNLOAD_URL"
+echo "download: $AMDURL"
+curl -L -o intelbin "$AMDURL"
+echo "download: $ARMURL"
+curl -L -o armbin "$ARMURL"
 # Need sha256sum ... install on mac via: 'brew install coreutils'
 # And then do 'ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum'
 which sha256sum
-sha256sum binary | cut -f 1 -d " " > SHA
+sha256sum intelbin | cut -f 1 -d " " > INTELSHA
+sha256sum armbin | cut -f 1 -d " " > ARMSHA
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  cat SHA | xargs -I NEWSHA sed -i '' 's/sha256 ".*"/sha256 "NEWSHA"/g' $FORMULA
+  cat INTELSHA | xargs -I NEWSHA sed -i '' -e "s|AMDSHAPH|NEWSHA|g" $FORMULA
+  cat ARMSHA | xargs -I NEWSHA sed -i '' -e "s|ARMSHAPH|NEWSHA|g" $FORMULA
 else
-  cat SHA | xargs -I NEWSHA sed -i'' 's/sha256 ".*"/sha256 "NEWSHA"/g' $FORMULA
+  cat INTELSHA | xargs -I NEWSHA sed -i'' -e "s|AMDSHAPH|NEWSHA|g" $FORMULA
+  cat ARMSHA | xargs -I NEWSHA sed -i'' -e "s|ARMSHAPH|NEWSHA|g" $FORMULA
 fi
 
 
