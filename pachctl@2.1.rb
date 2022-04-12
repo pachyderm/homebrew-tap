791a990c63d4daa549501b57ce8c98ec87669744
pachydermbuildbot <buildbot@pachyderm.io>
2022-04-12 17:19:45 +0000

    [Automated] Update formula to release version 2.1.5-ef106b6766de1fb2bb2f08558baaef5716668692

--- pachctl@2.1.rb
+++ pachctl@2.1.rb
@@ -5,9 +5,9 @@ require 'erb'
 
 class PachctlAT21 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v2.1.5/pachctl_2.1.5_darwin_amd64.zip"
-  sha256 "6549c6dd88b9eb1155c41602b12bc9f3544a00b0932878dfe7357efccaa36d4e"
-  version "v2.1.5"
+  url "https://github.com/pachyderm/pachyderm/releases/download/v2.1.5-ef106b6766de1fb2bb2f08558baaef5716668692/pachctl_2.1.5-ef106b6766de1fb2bb2f08558baaef5716668692_darwin_amd64.zip"
+  sha256 "b029e2aae8e79210a65aa48a5a7103062a814b069de23436161b48facc39cba9"
+  version "v2.1.5-ef106b6766de1fb2bb2f08558baaef5716668692"
 
   def install
     bin.install buildpath/"pachctl"
