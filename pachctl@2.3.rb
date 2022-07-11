ef198fa4ca059aba15997ede4e53984d91979393
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2022-07-11 08:59:52 -0400

    update 2.3 formula with arm

--- pachctl@2.3.rb
+++ pachctl@2.3.rb
@@ -5,13 +5,26 @@ require 'erb'
 
 class PachctlAT23 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  url "https://github.com/pachyderm/pachyderm/releases/download/v2.3.0-alpha.1/pachctl_2.3.0-alpha.1_darwin_amd64.zip"
-  sha256 "083a09b55ba81e68d8ce1e07336427f9a56b95a5b4fe8a4b972c5896ae2f3391"
   version "v2.3.0-alpha.1"
 
-  def install
-    bin.install buildpath/"pachctl"
+  if Hardware::CPU.intel?
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.3.0-alpha.1/pachctl_2.3.0-alpha.1_darwin_amd64.zip"
+    sha256 "083a09b55ba81e68d8ce1e07336427f9a56b95a5b4fe8a4b972c5896ae2f3391"
+
+    def install
+      bin.install buildpath/"pachctl"
+    end
   end
+  if Hardware::CPU.arm?
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.3.0-alpha.1/pachctl_2.3.0-alpha.1_darwin_arm64.zip"
+    sha256 "03dd358f8f348de8261da010b70de0690fcb8b2eb6d1e7090876bb6a309295ea"
+
+    def install
+      bin.install buildpath/"pachctl"
+    end
+  end
+
+
 
   test do
     system "#{bin}/pachctl version"
