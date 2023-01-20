2d985c6be3253e088620b9d1e0d8d446344f3dff
pachydermbuildbot <buildbot@pachyderm.io>
2023-01-20 15:44:42 +0000

    [Automated] Update formula to release version 2.5.0-alpha.4

--- pachctl.rb
+++ pachctl.rb
@@ -3,21 +3,21 @@ require "language/go"
 require 'erb'
 
 
-class Pachctl < Formula
+class PachctlAT25 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.4.4"
+  version "v2.5.0-alpha.4"
 
   if Hardware::CPU.intel?
-    url "https://github.com/pachyderm/pachyderm/releases/download/v2.4.4/pachctl_2.4.4_darwin_amd64.zip"
-    sha256 "b93a1aa4d12818cba6ccbf9691a19c5085043fdeac1aba9646e7b5328e0e1a9e"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.5.0-alpha.4/pachctl_2.5.0-alpha.4_darwin_amd64.zip"
+    sha256 "aa307c82c93ba30091beef9f95401a3ee56087810975c4af624f96b981e5f9b2"
 
     def install
       bin.install buildpath/"pachctl"
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/pachyderm/pachyderm/releases/download/v2.4.4/pachctl_2.4.4_darwin_arm64.zip"
-    sha256 "17e105f8eaac3b625c725a86644f98bdebf3dd490915f8a94a9c3d8575ca090f"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.5.0-alpha.4/pachctl_2.5.0-alpha.4_darwin_arm64.zip"
+    sha256 "a5271995f2a4b6cfa575e243feb081503195143583b15138b12e2cebb9887c6a"
 
     def install
       bin.install buildpath/"pachctl"
--- pachctl@2.5.rb
+++ pachctl@2.5.rb
@@ -5,19 +5,19 @@ require 'erb'
 
 class PachctlAT25 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.5.0-alpha.3"
+  version "v2.5.0-alpha.4"
 
   if Hardware::CPU.intel?
-    url "https://github.com/pachyderm/pachyderm/releases/download/v2.5.0-alpha.3/pachctl_2.5.0-alpha.3_darwin_amd64.zip"
-    sha256 "df3a8e68fcf773ddd5b59de5fd48a22dc2d828f03882002064972ab02de10bcc"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.5.0-alpha.4/pachctl_2.5.0-alpha.4_darwin_amd64.zip"
+    sha256 "aa307c82c93ba30091beef9f95401a3ee56087810975c4af624f96b981e5f9b2"
 
     def install
       bin.install buildpath/"pachctl"
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/pachyderm/pachyderm/releases/download/v2.5.0-alpha.3/pachctl_2.5.0-alpha.3_darwin_arm64.zip"
-    sha256 "f96ee721665e55c065d336773c496e0ddaf19a4a3a3d27cdffca88336750c395"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.5.0-alpha.4/pachctl_2.5.0-alpha.4_darwin_arm64.zip"
+    sha256 "a5271995f2a4b6cfa575e243feb081503195143583b15138b12e2cebb9887c6a"
 
     def install
       bin.install buildpath/"pachctl"
