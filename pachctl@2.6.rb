dbb668b1541c437f73d59f0e9452f4dd6c4a89cd
pachydermbuildbot <buildbot@pachyderm.io>
2023-05-11 08:55:32 +0000

    [Automated] Update formula to release version 2.6.0-nightly.20230511

--- pachctl@2.6.rb
+++ pachctl@2.6.rb
@@ -5,19 +5,19 @@ require 'erb'
 
 class PachctlAT26 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.6.0-rc.2"
+  version "v2.6.0-nightly.20230511"
 
   if Hardware::CPU.intel?
-    url "https://github.com/pachyderm/pachyderm/releases/download/v2.6.0-rc.2/pachctl_2.6.0-rc.2_darwin_amd64.zip"
-    sha256 "d8af90cbd4bff0981db07930d811c4afb065875d8296f415becb9848206d1b63"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.6.0-nightly.20230511/pachctl_2.6.0-nightly.20230511_darwin_amd64.zip"
+    sha256 "9f8f2aa21468cc09a69e99aa9f4cb4f943551ee9009a9533dd82e26a0e2efd9c"
 
     def install
       bin.install buildpath/"pachctl"
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/pachyderm/pachyderm/releases/download/v2.6.0-rc.2/pachctl_2.6.0-rc.2_darwin_arm64.zip"
-    sha256 "2339ce2679d225c9cd57291301694669f3d33ef1eef6dc753c5d64916681346f"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.6.0-nightly.20230511/pachctl_2.6.0-nightly.20230511_darwin_arm64.zip"
+    sha256 "d01a64d8785b415e8af723c027e041de587ded408c0112e668be4fb4a7d7b6d6"
 
     def install
       bin.install buildpath/"pachctl"
