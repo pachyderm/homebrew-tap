dae53fb7d2ef69b5bd60807097607f566b13e739
Jose Molina-Melendez <molinamelendezj@gmail.com>
2023-08-23 16:16:26 -0400

    fix latest pachctl homebrew tap

--- pachctl.rb
+++ pachctl.rb
@@ -3,21 +3,21 @@ require "language/go"
 require 'erb'
 
 
-class PachctlAT26 < Formula
+class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.6.10"
+  version "v2.7.0"
 
   if Hardware::CPU.intel?
-    url "https://github.com/pachyderm/pachyderm/releases/download/v2.6.10/pachctl_2.6.10_darwin_amd64.zip"
-    sha256 "450d68359ee2efafa4dcad61d4ac945adc535fa7520a1e1abb7c9a20155a7ca1"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.7.0/pachctl_2.7.0_darwin_amd64.zip"
+    sha256 "d4846fe3d26e2e3d77d86588ae79f8dc5a51bda74800f5107ac28db3545202b7"
 
     def install
       bin.install buildpath/"pachctl"
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/pachyderm/pachyderm/releases/download/v2.6.10/pachctl_2.6.10_darwin_arm64.zip"
-    sha256 "bb12422f5412b153b07af1a6a2a75d147a90c6a7a8e818284d290ebdfd1cb4f5"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v2.7.0/pachctl_2.7.0_darwin_arm64.zip"
+    sha256 "097b3c95541d3a92b9b9a85bc3cde05e064af12749c0bdc2f84af3040d65b26c"
 
     def install
       bin.install buildpath/"pachctl"
