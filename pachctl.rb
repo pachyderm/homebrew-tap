3f12d81e192e0b02ad1e09e9a1222cfd5f82c8e6
Jose Molina-Melendez <molinamelendezj@gmail.com>
2024-04-10 17:18:08 -0400

    Update formula to release version 2.9.4

--- pachctl.rb
+++ pachctl.rb
@@ -2,31 +2,31 @@ require "formula"
 require "language/go"
 require "erb"
 
-class PachctlAT27 < Formula
+class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.7.8"
+  version "v2.9.4"
 
   on_macos do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.7.8/pachctl_2.7.8_darwin_arm64.zip"
-      sha256 "162d1e689d5a7f50fc93368c4125da09178d82c36b3b5bf7896eecc87a979c63"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.4/pachctl_2.9.4_darwin_arm64.zip"
+      sha256 "935c0c60c67ae2d743100077bb8feec517b5a879f70e2b9409e026b4dec16f13"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.7.8/pachctl_2.7.8_darwin_amd64.zip"
-      sha256 "26efeca26602d304d9a21107b0175d307cf2eee2d9193e78e7f4f55b8dcc800d"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.4/pachctl_2.9.4_darwin_amd64.zip"
+      sha256 "015d000885493ae873db180f6dbae580e9fb9640f68b4b55216a36fd52d7a63e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.7.8/pachctl_2.7.8_linux_arm64.tar.gz"
-      sha256 "de2e061d345fb9d93de7650be02eaf2b9c7ab549b86bc6d7ca0a4c9ba671c66d"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.4/pachctl_2.9.4_linux_arm64.tar.gz"
+      sha256 "f0c103ee91d70ba664f572c0112dd73e31a1f10acd4f97d4bd61bea930d75020"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.7.8/pachctl_2.7.8_linux_amd64.tar.gz"
-      sha256 "3b36190f08a2cac6baadef87eb6a6507b313b283d06380e0edf6f73168834379"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.9.4/pachctl_2.9.4_linux_amd64.tar.gz"
+      sha256 "0651ef7cba5cc1d6ecae679a57c2c3e767afe83403375161d29ce34662528ed8"
     end
   end
 
