50501497f30be4dea1f79ac562366588c99595bd
pachydermbuildbot <buildbot@pachyderm.io>
2024-04-25 18:42:04 +0000

    [Automated] Update formula to release version 2.10.0-rc.1

--- pachctl@2.10.rb
+++ pachctl@2.10.rb
@@ -4,29 +4,29 @@ require "erb"
 
 class PachctlAT210 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.10.0-alpha.5"
+  version "v2.10.0-rc.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-alpha.5/pachctl_2.10.0-alpha.5_darwin_arm64.zip"
-      sha256 "2aca4af4d002cadeaf6e4e41c5153685f6e23f0efd07defed60cc71761ddf74d"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.1/pachctl_2.10.0-rc.1_darwin_arm64.zip"
+      sha256 "3d439e224f18e96fe4e12354b3e42a31c83aad95289cf361bc2d74a65d96b79b"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-alpha.5/pachctl_2.10.0-alpha.5_darwin_amd64.zip"
-      sha256 "f392d98163600449f4bb7e0237c117e1cf891ec721525f6cf1bb2997d89d903e"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.1/pachctl_2.10.0-rc.1_darwin_amd64.zip"
+      sha256 "29d990b6348bbb8e57871192297094f0a489df7bbe731b9252e5fa4e1543cd70"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-alpha.5/pachctl_2.10.0-alpha.5_linux_arm64.tar.gz"
-      sha256 "bf2b73941fab5554c1dd9866a43ae2252c21cd62771a69cfe148a80f3282f74c"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.1/pachctl_2.10.0-rc.1_linux_arm64.tar.gz"
+      sha256 "6b2e9eb1ac5ff36f2ee90324cb433de2b8dfb13aa7bdc7dd4aa61b5ca690a66b"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-alpha.5/pachctl_2.10.0-alpha.5_linux_amd64.tar.gz"
-      sha256 "0675a455dfa5de8cde5935b72d8ef299da3b3399774e7c92972561311a077ea6"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.1/pachctl_2.10.0-rc.1_linux_amd64.tar.gz"
+      sha256 "ace56361814e015b5fa3fd4c58cac4a81c357eddbc9947b1a9f2929ac8980ec9"
     end
   end
 
