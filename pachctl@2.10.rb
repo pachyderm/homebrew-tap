c3d23949e366920b302c0d960f8295ecc36eee85
pachydermbuildbot <buildbot@pachyderm.io>
2024-05-13 16:40:14 +0000

    [Automated] Update formula to release version 2.10.0-rc.3

--- pachctl@2.10.rb
+++ pachctl@2.10.rb
@@ -4,29 +4,29 @@ require "erb"
 
 class PachctlAT210 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.10.0-rc.2"
+  version "v2.10.0-rc.3"
 
   on_macos do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.2/pachctl_2.10.0-rc.2_darwin_arm64.zip"
-      sha256 "e905fe3e50cac0df66ebcdc6cce805316299a88c5fd191bb0ea8e05a77751e00"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.3/pachctl_2.10.0-rc.3_darwin_arm64.zip"
+      sha256 "6c377621f8431060a11105fa89df1a06f310db4f124646c6874caae6da4da567"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.2/pachctl_2.10.0-rc.2_darwin_amd64.zip"
-      sha256 "e40dae9ef4fd5b81ca7e361b33ee28f5072e9392b9f6f1eada54306bc944659d"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.3/pachctl_2.10.0-rc.3_darwin_amd64.zip"
+      sha256 "f3e9d250285a5236480a21a2f864bd3855b95ef5245aaf3fde1079015d7d57b4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.2/pachctl_2.10.0-rc.2_linux_arm64.tar.gz"
-      sha256 "6bcd76f305558bc3fd1001107e63d500adfe0068e5b54394f64a42a1eb56bbbe"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.3/pachctl_2.10.0-rc.3_linux_arm64.tar.gz"
+      sha256 "4c0d1b190fb4c884a0a3f55f3ad798e777d43259e24c6c3f346be7b8d4109eac"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.2/pachctl_2.10.0-rc.2_linux_amd64.tar.gz"
-      sha256 "5be8571eb0f548810bde5b8dd9b93ea65c337659c40ec41bec0174cf6f1e2eb5"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.0-rc.3/pachctl_2.10.0-rc.3_linux_amd64.tar.gz"
+      sha256 "378cc318d1701615844b01db25c529778cbf6570434c69604925b95f429dfb92"
     end
   end
 
