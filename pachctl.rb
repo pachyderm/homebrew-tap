a23115dd59f4682e7ae9a1489d5a37a096bb0251
pachydermbuildbot <buildbot@pachyderm.io>
2024-06-05 20:00:20 +0000

    [Automated] Update formula to release version 2.10.2

--- pachctl.rb
+++ pachctl.rb
@@ -4,29 +4,29 @@ require "erb"
 
 class Pachctl < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.10.1"
+  version "v2.10.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.1/pachctl_2.10.1_darwin_arm64.zip"
-      sha256 "9a678360eaaa0628f028ce0cd1281d71d77789a5186839c145421be261aad9f0"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.2/pachctl_2.10.2_darwin_arm64.zip"
+      sha256 "e41bf69bfd1812ca22e7a4201f2ad24bb4bf69bff5245a15c178312ec73d63f2"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.1/pachctl_2.10.1_darwin_amd64.zip"
-      sha256 "03db2c4217c8bf692d7dec0868c78db5b525a24647e624e2c4633e86b772e78d"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.2/pachctl_2.10.2_darwin_amd64.zip"
+      sha256 "6ad4c4bae4c7a48494047f0672de1e72d05f8d5efec2e7a59a78f599130fccbb"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.1/pachctl_2.10.1_linux_arm64.tar.gz"
-      sha256 "c9c13030a9fb76adf837144249d396913d0ca7598f09e8bfbeb6b11e0629a93f"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.2/pachctl_2.10.2_linux_arm64.tar.gz"
+      sha256 "b13109a6f0538a49b3701ddd10709ec1c9aa7c57a91ac0b59b6abb4a64d1fbda"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.1/pachctl_2.10.1_linux_amd64.tar.gz"
-      sha256 "ffed18d721b161d9f8ee22c040c94bfc3d97553157d01d53af3bbc3d7f6322ab"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.2/pachctl_2.10.2_linux_amd64.tar.gz"
+      sha256 "7ddfa145b167b59d7b9b677a59af45ab3d5bc89394e37f047198de5156707170"
     end
   end
 
