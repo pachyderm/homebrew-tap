b5cf9454e79ab57f1073d8bca4f9034dc07e88ca
pachydermbuildbot <buildbot@pachyderm.io>
2024-06-05 16:25:14 +0000

    [Automated] Update formula to release version 2.8.10

--- pachctl@2.8.rb
+++ pachctl@2.8.rb
@@ -4,29 +4,29 @@ require "erb"
 
 class PachctlAT28 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.8.9"
+  version "v2.8.10"
 
   on_macos do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.8.9/pachctl_2.8.9_darwin_arm64.zip"
-      sha256 "354340ed40b6420c6bd4fbdcf7fdbb2732314a98dd0a42588bf2edc9ababb0bd"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.8.10/pachctl_2.8.10_darwin_arm64.zip"
+      sha256 "400c989a6cf075b2a796c2b70748df28b236bb6ce5189e330b88503cd93bb172"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.8.9/pachctl_2.8.9_darwin_amd64.zip"
-      sha256 "fd55015ceeec985b1e85bad1213758675791797bbca2613d0591875be0a5e920"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.8.10/pachctl_2.8.10_darwin_amd64.zip"
+      sha256 "a59bbbd5ab7fe53d25c8bd11532a3445aac0e6b336da04d5069834cb1f644389"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.8.9/pachctl_2.8.9_linux_arm64.tar.gz"
-      sha256 "46480d5f7ca15ea3e5eb4b850309d4fdea50111d86c58fb30ed12618890d0d6b"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.8.10/pachctl_2.8.10_linux_arm64.tar.gz"
+      sha256 "5d8fc36a9901e8f54e8b04392301d21d2a368a49518694dab881375afaef796d"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.8.9/pachctl_2.8.9_linux_amd64.tar.gz"
-      sha256 "cfbea02e5f01216d7b0752fbe5b2cdda3d5909f7419c2bf4a563ffec54c755f9"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.8.10/pachctl_2.8.10_linux_amd64.tar.gz"
+      sha256 "6f05391fe2413276b4e0906ae620868dd4d296343906818779e687e84f5cdbbc"
     end
   end
 
