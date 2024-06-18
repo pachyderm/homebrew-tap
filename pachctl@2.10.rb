26e0ec45d4bfcce47814353edbd8235054ff184a
pachydermbuildbot <buildbot@pachyderm.io>
2024-06-18 18:46:06 +0000

    [Automated] Update formula to release version 2.10.4

--- pachctl@2.10.rb
+++ pachctl@2.10.rb
@@ -4,29 +4,29 @@ require "erb"
 
 class PachctlAT210 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.10.2"
+  version "v2.10.4"
 
   on_macos do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.2/pachctl_2.10.2_darwin_arm64.zip"
-      sha256 "e41bf69bfd1812ca22e7a4201f2ad24bb4bf69bff5245a15c178312ec73d63f2"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.4/pachctl_2.10.4_darwin_arm64.zip"
+      sha256 "0b9ec341378df020409ae2f86d5b2ee47125cd272aeb410a5518c0287fef3d07"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.2/pachctl_2.10.2_darwin_amd64.zip"
-      sha256 "6ad4c4bae4c7a48494047f0672de1e72d05f8d5efec2e7a59a78f599130fccbb"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.4/pachctl_2.10.4_darwin_amd64.zip"
+      sha256 "55bad8538ad8d6d7865803f99b435eaabccdf3027a9878ffe49dd6d10849c30d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.2/pachctl_2.10.2_linux_arm64.tar.gz"
-      sha256 "b13109a6f0538a49b3701ddd10709ec1c9aa7c57a91ac0b59b6abb4a64d1fbda"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.4/pachctl_2.10.4_linux_arm64.tar.gz"
+      sha256 "36505351243ca047a3adad81e2211b49984d1c01c9f6557545c24bf31552cddb"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.2/pachctl_2.10.2_linux_amd64.tar.gz"
-      sha256 "7ddfa145b167b59d7b9b677a59af45ab3d5bc89394e37f047198de5156707170"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.10.4/pachctl_2.10.4_linux_amd64.tar.gz"
+      sha256 "ea99ced63b9fd515f0624762a6db9eda19fb6add6e9e04bce815fcb8933cb2be"
     end
   end
 
