111fb6766ae6f554b4a0e1247d1f5b7948ebdf67
pachydermbuildbot <buildbot@pachyderm.io>
2024-07-17 19:55:54 +0000

    [Automated] Update formula to release version 2.11.0-alpha.6

--- pachctl@2.11.rb
+++ pachctl@2.11.rb
@@ -4,29 +4,29 @@ require "erb"
 
 class PachctlAT211 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v2.11.0-alpha.5"
+  version "v2.11.0-alpha.6"
 
   on_macos do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.11.0-alpha.5/pachctl_2.11.0-alpha.5_darwin_arm64.zip"
-      sha256 "b0f8c236a7f40da0a85e8fc6152d95d1cd50b2f447639181bb0af714bce7bdc2"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.11.0-alpha.6/pachctl_2.11.0-alpha.6_darwin_arm64.zip"
+      sha256 "adc0c54f997c915fbaf33b5c4123fe842b1490919e628528c76bd9ff77b7c784"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.11.0-alpha.5/pachctl_2.11.0-alpha.5_darwin_amd64.zip"
-      sha256 "2d93aecdc0624ff61d32226d0bde3b81c5324a88a6798d3e63fa141862a51e1e"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.11.0-alpha.6/pachctl_2.11.0-alpha.6_darwin_amd64.zip"
+      sha256 "8972143cee6a9bb686d354458981d16dca08de67924f3fb63ae1b27b3e9d1465"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.11.0-alpha.5/pachctl_2.11.0-alpha.5_linux_arm64.tar.gz"
-      sha256 "835b93ce32b27f90bf597cb791b05fe6d793277017a1fe36df6a215263264a47"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.11.0-alpha.6/pachctl_2.11.0-alpha.6_linux_arm64.tar.gz"
+      sha256 "6b210a6fe7dd3ed42c815141d07d37644d8d1501d531d0f854b0b993b4a4dfcf"
     end
 
     on_intel do
-      url "https://github.com/pachyderm/pachyderm/releases/download/v2.11.0-alpha.5/pachctl_2.11.0-alpha.5_linux_amd64.tar.gz"
-      sha256 "05f2cd07ab88ef294e048c2fff554fc64c6694df7abaadc4eb78a3dc12afebb0"
+      url "https://github.com/pachyderm/pachyderm/releases/download/v2.11.0-alpha.6/pachctl_2.11.0-alpha.6_linux_amd64.tar.gz"
+      sha256 "44b89d8a5e96585ee293c721a437af956518467b7fd9bf92c5985d0ef320687d"
     end
   end
 
