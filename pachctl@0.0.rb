7afc9766ae2b812c343452b8ddfc7dd604ffd9bf
pachydermbuildbot <buildbot@pachyderm.io>
2024-07-30 21:30:41 +0000

    [Automated] Update formula to release version 0.0.0-test.4

--- pachctl@0.0.rb
+++ pachctl@0.0.rb
@@ -4,16 +4,16 @@ require "erb"
 
 class PachctlAT00 < Formula
   homepage "github.com/pachyderm/pachyderm"
-  version "v0.0.0-test.3"
+  version "v0.0.0-test.4"
 
   on_macos do
-    url "https://github.com/pachyderm/pachyderm/releases/download/v0.0.0-test.3/pachctl_0.0.0-test.3_darwin_amd64.zip"
-    sha256 "cc53229589cde41372dd9c2fb548f6cc5af2318aaa4d4aa7c64c79a408bc4994"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v0.0.0-test.4/pachctl_0.0.0-test.4_darwin_amd64.zip"
+    sha256 "0d41a80fa228046dfb2124e767bd0b2c274fefcd9823099a2669d02380155b4e"
   end
 
   on_linux do
-    url "https://github.com/pachyderm/pachyderm/releases/download/v0.0.0-test.3/pachctl_0.0.0-test.3_linux_amd64.tar.gz"
-    sha256 "c89f50e689c3fedf412a623bfee3d83ed0b7cd8372a56314dbd1dcbbd7e4f6e0"
+    url "https://github.com/pachyderm/pachyderm/releases/download/v0.0.0-test.4/pachctl_0.0.0-test.4_linux_amd64.tar.gz"
+    sha256 "7d055744dbfc8d7fd13f44d83100d03303c756752df63c4d959face974cf9731"
   end
 
   def install
